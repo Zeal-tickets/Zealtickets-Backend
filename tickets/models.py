f3dbf10548f99c1625c8326d5dd4b32e63efee16
calvinarieri <ariericalvin@gmail.com>
2024-08-03 22:09:22 +0300

    finished modeling tickets and transaction tables

--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from events.models import Event
+from payment.models import Transaction
 
-class Ticket()
+class Ticket(models.Model):
+    amount = models.IntegerField()
+    ticket_type = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    number = models.IntegerField()
+    event = models.ForeignKey(Event , on_delete=models.SET_NULL , null=True)
+
+
+class Purchase(models.Model):
+    datetime = models.DateTimeField()
+    payment_method = models.CharField(max_length=1000)
+    UUID = models.CharField(max_length=300)
+    status = models.CharField(max_length=50)
+    
+    transaction = models.ForeignKey(Transaction, on_delete=models.SET_NULL, null=True)
+
+    ticket = models.ForeignKey(Ticket, on_delete=models.SET_NULL, null=True)
--- zealtickets/settings.py
+++ zealtickets/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     # Personal apps
     'accounts',
     'events',
+    'payment',
+    'tickets',
+    'authentication',
 ]
 
 MIDDLEWARE = [
